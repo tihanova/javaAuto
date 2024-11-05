0fc76be9250b40fda0240512948af2078fd3e5bf
a.tihanova <a.tihanova@cdek.ru>
2024-11-05 17:16:05 +0300

    Дз 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int birth = 1992;
+        int nextYear = 2034;
+
+        System.out.printf("В 2034 мне будет %s!", nextYear - birth );
+    }
+}
